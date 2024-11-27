604f6875bae528b79ca923bf17151b3e4c43556b
map <76199586+Maploop@users.noreply.github.com>
2024-11-27 09:25:32 +0330

    config changes and add NOICE.nvim

--- lua/maploop/init.lua
+++ lua/maploop/init.lua
@@ -1,9 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>c", vim.cmd.FloatermToggle)
 vim.keymap.set("n", "<leader>z", vim.cmd.undo)
-vim.keymap.set("i", "<leader>z", vim.cmd.undo)
 vim.keymap.set("n", "<leader>u", vim.cmd.redo)
-vim.keymap.set("i", "<leader>u", vim.cmd.redo)
 vim.keymap.set("n", "<leader>q", vim.cmd.quit)
 -- vim.keymap.set("n", "<leader>e", vim.cmd.Neotree)
-vim.cmd "colo tokyodark"
+vim.cmd "colo catppuccin-mocha"
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,4 @@
 return {
   "tiagovla/tokyodark.nvim",
+  "catppuccin/nvim",
 }
